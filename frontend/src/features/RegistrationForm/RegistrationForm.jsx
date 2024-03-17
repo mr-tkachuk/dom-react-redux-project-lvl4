52c01aa9d54a27fcfac93093c66accc2f4262340
Tkachuk Anton <mrtkachuk@inbox.ru>
2024-03-17 17:19:30 +0300

    fix 1 test

--- frontend/src/features/RegistrationForm/RegistrationForm.jsx
+++ frontend/src/features/RegistrationForm/RegistrationForm.jsx
@@ -1,4 +1,4 @@
-import { Button, FloatingLabel, Form } from 'react-bootstrap';
+import { Button, Form } from 'react-bootstrap';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import { useDispatch, useSelector } from 'react-redux';
@@ -39,68 +39,74 @@ export default function RegistrationForm() {
           >
             <h1 className="text-center mb-4">{t('signUp')}</h1>
             <Form.Group
-              controlId="name"
               className="position-relative mb-3"
             >
-              <FloatingLabel
+              <Form.Label
                 label={t('username')}
+                htmlFor="name"
               >
-                <Form.Control
-                  autoFocus
-                  type="text"
-                  name="name"
-                  placeholder={t('username')}
-                  value={values.name}
-                  isInvalid={(touched.name && !!errors.name) || error}
-                  onChange={handleChange}
-                  autoComplete="username"
-                />
-                <Form.Control.Feedback tooltip type="invalid">{errors.name || error}</Form.Control.Feedback>
-              </FloatingLabel>
+                {t('username')}
+              </Form.Label>
+              <Form.Control
+                autoFocus
+                type="text"
+                name="name"
+                id="name"
+                placeholder={t('username')}
+                value={values.name}
+                isInvalid={(touched.name && !!errors.name) || error}
+                onChange={handleChange}
+                autoComplete="username"
+              />
+              <Form.Control.Feedback tooltip type="invalid">{errors.name || error}</Form.Control.Feedback>
             </Form.Group>
             <Form.Group
-              controlId="password"
               className="position-relative mb-3"
             >
-              <FloatingLabel
+              <Form.Label
                 label={t('password')}
+                htmlFor="password"
               >
-                <Form.Control
-                  type="password"
-                  name="password"
-                  placeholder={t('password')}
-                  value={values.password}
-                  onChange={handleChange}
-                  isInvalid={touched.password && !!errors.password}
-                  autoComplete="new-password"
-                  required
-                />
-                <Form.Control.Feedback tooltip type="invalid">
-                  {errors.password}
-                </Form.Control.Feedback>
-              </FloatingLabel>
+                {t('password')}
+              </Form.Label>
+              <Form.Control
+                type="password"
+                name="password"
+                placeholder={t('password')}
+                value={values.password}
+                onChange={handleChange}
+                id="password"
+                isInvalid={touched.password && !!errors.password}
+                autoComplete="new-password"
+                required
+              />
+              <Form.Control.Feedback tooltip type="invalid">
+                {errors.password}
+              </Form.Control.Feedback>
             </Form.Group>
             <Form.Group
-              controlId="confirmPassword"
               className="position-relative mb-3"
             >
-              <FloatingLabel
+              <Form.Label
                 label={t('confirmPassword')}
+                htmlFor="confirmPassword"
               >
-                <Form.Control
-                  type="password"
-                  name="confirmPassword"
-                  placeholder={t('confirmPassword')}
-                  value={values.confirmPassword}
-                  isInvalid={touched.confirmPassword && !!errors.confirmPassword}
-                  onChange={handleChange}
-                  autoComplete="new-password"
-                  required
-                />
-                <Form.Control.Feedback tooltip type="invalid">
-                  {errors.confirmPassword}
-                </Form.Control.Feedback>
-              </FloatingLabel>
+                {t('confirmPassword')}
+              </Form.Label>
+              <Form.Control
+                type="password"
+                name="confirmPassword"
+                placeholder={t('confirmPassword')}
+                value={values.confirmPassword}
+                id="confirmPassword"
+                isInvalid={touched.confirmPassword && !!errors.confirmPassword}
+                onChange={handleChange}
+                autoComplete="new-password"
+                required
+              />
+              <Form.Control.Feedback tooltip type="invalid">
+                {errors.confirmPassword}
+              </Form.Control.Feedback>
             </Form.Group>
             <Button className="w-100 mb-3" variant="outline-primary" type="submit">
               {t('toSignUp')}
