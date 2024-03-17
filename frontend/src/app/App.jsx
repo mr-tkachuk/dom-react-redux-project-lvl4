4aae4c4889936f349043e5316e969b8a73e13ce3
Tkachuk Anton <mrtkachuk@inbox.ru>
2024-03-17 10:14:59 +0300

    complete 11 step

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -5,12 +5,14 @@ import {
   RouterProvider,
 } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import Main from '../pages/Main';
 import Login from '../pages/Login';
 import NotFound from '../pages/NotFound';
 import Root from './Root';
 import Registration from '../pages/Registration';
 import store from './store';
+import rollbarConfig from './rollbarConfig';
 
 const router = createBrowserRouter([
   {
@@ -36,9 +38,13 @@ const router = createBrowserRouter([
 
 function App() {
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <RouterProvider router={router} />
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   );
 }
 
---
+++ frontend/src/app/rollbarConfig.js
@@ -0,0 +1,4 @@
+export default {
+  accessToken: 'c7a4a5c22205409492292c14b5fc5a6b',
+  environment: 'production',
+};
